51e206fd984c31c7025bd9cd6e7bac96bb9d1be8
hypecryte <hypecryte@gmail.com>
2026-05-18 19:39:13 +0300

    adding shutdown function

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-import os, re
+import os, re, time
+
+
+def shutdown():
+    time_text = input("Введите время: ")
+    # Получаем время из текста
+    hours = r"\d{1,}(?=[Чч])"
+    minuts = r"\d{1,}(?=[Мм])"
+    seconds = r"\d{1,}(?=[Сс])"
+    # Конвертируем итоговое время
+    result_time = (hours * 60 * 60) + minuts * 60 + seconds
+    print(result_time)
+    time.sleep()
+
 
 if __name__ == "__main__":
     main()
